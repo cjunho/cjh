9f8cf191ff371dd7b013e9d989aa0ae2f6fbdc11
cjunho <47020816+cjunho@users.noreply.github.com>
2022-02-12 01:12:54 +0900

    Add files via upload

---
+++ call_energy.py
@@ -0,0 +1,39 @@
+"Energy evolution against time"
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+soliton_number="SP3"     # choices=["SP2", "SP3"]
+#SP2: Two solitons interaction;
+#SP3: Three solitons interaction
+
+if soliton_number=="SP3":# initial time t0 
+    t0=-200
+else:
+    t0=0    
+    
+""" ________________ Time variable ________________ """
+dt=0.005                  # time step
+T = .1                    # duration time
+tt=np.arange(t0,t0+.1,dt) # time variable
+
+""" ________________ Recall energy ________________ """
+df = pd.read_csv(r"data/energy.csv")
+energy=df.to_numpy()
+energy=energy-energy[0] # E(t)-E(t_0)
+
+
+""" ________________ plot evolution ________________ """
+plt.title(r"E(t)-E(%i)" %t0, fontsize=20) 
+plt.plot(tt,energy,linewidth=5,color='black')
+plt.xticks(fontsize =13,weight='bold')
+plt.yticks(fontsize =13,weight='bold')
+plt.ticklabel_format( axis='y', style='sci',scilimits=(-3,1))
+plt.xlabel("time", fontsize=15,weight='bold')
+plt.grid()
+
+plt.savefig('data/energy.eps')
+plt.show()
+
---
+++ call_maximum.py
@@ -0,0 +1,39 @@
+"Energy evolution against time"
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+soliton_number="SP3"     # choices=["SP2", "SP3"]
+#SP2: Two solitons interaction;
+#SP3: Three solitons interaction
+
+if soliton_number=="SP3":# initial time t0 
+    t0=-200
+else:
+    t0=0    
+    
+""" ________________ Time variable ________________ """
+dt=0.005                  # time step
+T = .1                    # duration time
+tt=np.arange(t0,t0+.1,dt) # time variable
+
+""" ________________ Recall energy ________________ """
+df = pd.read_csv(r"data123/max.csv")
+max1=df.to_numpy()
+
+
+
+""" ________________ plot evolution ________________ """
+plt.title(r"Maximum", fontsize=20) 
+plt.plot(tt,max1,linewidth=5,color='black')
+plt.xticks(fontsize =13,weight='bold')
+plt.yticks(fontsize =13,weight='bold')
+plt.ticklabel_format( axis='y', style='sci',scilimits=(-3,1))
+plt.xlabel("time", fontsize=15,weight='bold')
+plt.grid()
+
+plt.savefig('data123/maximum.eps')
+plt.show()
+
