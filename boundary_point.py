2030e473e841ae864be65b981893fbb22eada22e
cjunho <47020816+cjunho@users.noreply.github.com>
2022-02-11 21:05:11 +0900

    Update boundary_point.py

--- boundary_point.py
+++ boundary_point.py
@@ -1,7 +1,9 @@
 "This file computes some valuses to design a computaional domain."
-"• Function 'bd_x1' computes the value of the left boundary of the domain."
-"• Function 'bd_x2' computes the value of the right boundary of the domain."
-"• Function 'yast' computes yast such that eta is symmetric about y=yast"
+"* Function 'bd_x1' computes the value of the left boundary of the domain for SP3."
+"* Function 'bd_x2' computes the value of the right boundary of the domain for SP3."
+"* Function 'bd_x21' computes the value of the left boundary of the domain for SP2."
+"* Function 'bd_x22' computes the value of the right boundary of the domain for SP2."
+"* Function 'yast' computes yast such that eta is symmetric about y=yast for SP3."
 "  so as to provide the value of the bottom boundary."
 
 import numpy as np
@@ -22,6 +24,18 @@ def bd_x2(yy,x_shift,k1,k2,k3,k4,k5,k6,t,ep,mu):
     
     return x2
 
+def bd_x21(yy,x_shift,k1,k2,k3,k4,t,ep,mu):
+    x1=x_shift-k4*(4.5)**(1/6)*ep**.5*yy-(mu/ep)**.5*np.log(2)/(4.5)**(1/6)/k4    
+    
+    return x1
+
+def bd_x22(yy,x_shift,k1,k2,k3,k4,t,ep,mu):
+    x2=x_shift-k1*(4.5)**(1/6)*ep**.5*yy-(mu/ep)**.5*np.log(2)/(4.5)**(1/6)/k1
+    
+    return x2
+
 def yast(k1,k2,k3,k4,k5,k6):    
     yast=(2/9)**(1/3)*.5*np.log(k5*(k5**2-k4**2)/(k6*(k6**2-k4**2)))/(k6**2-k5**2)
     return yast
+    
+
