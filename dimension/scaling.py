af4ebcad575c5787118d4c086916dcced9673c30
annakalog9 <99676190+annakalog9@users.noreply.github.com>
2022-02-17 10:20:09 +0000

    Tidying up the code

--- dimension/scaling.py
+++ dimension/scaling.py
@@ -1,11 +1,11 @@
 from firedrake import *
 
 def scaling_to_nondim(t0,ds,tan0,tildeA,Ly,H0,ep):
-    t0 = t0/(H0/9.8/ep)**.5                 # initial time in seconds
-    dt = ds/(H0/9.8/ep)**.5
+    t0 = t0/(H0/9.8/ep)**0.5                # initial time in seconds
+    dt = ds/(H0/9.8/ep)**0.5
     tan0 = tan0                             # angle 
     tildeA = tildeA/((4/3)**(1/3)*H0*ep)    # amplitude
-    Ly = Ly/(H0/ep**.5)
+    Ly = Ly/(H0/ep**0.5)
     
     return(t0,dt,tan0,tildeA,Ly)
 
