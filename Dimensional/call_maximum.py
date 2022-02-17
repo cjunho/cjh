520cb47670b72d7a12663a9f6fcf7f8e836b600c
cjunho <47020816+cjunho@users.noreply.github.com>
2022-02-18 01:07:43 +0900

    Create call_maximum.py

---
+++ Dimensional/call_maximum.py
@@ -0,0 +1,24 @@
+"Maximum evolution of eta against time"
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+""" ________________ Recall maximum and time ________________ """
+df = pd.read_csv("data/max.csv")
+max1 = df.to_numpy()
+
+df2 = pd.read_csv("data/time.csv")
+tt = df2.to_numpy()
+
+""" ________________ Plot evolution of maximum ________________ """
+plt.title("Maximum", fontsize=20) 
+plt.plot(tt, max1, linewidth=5, color='black')
+plt.xticks(fontsize=13, weight='bold')
+plt.yticks(fontsize=13, weight='bold')
+plt.ticklabel_format(axis='y', style='sci', scilimits=(-3,1))
+plt.xlabel("time", fontsize=15, weight='bold')
+plt.grid()
+
+plt.savefig('data123/maximum.eps')
+plt.show()
