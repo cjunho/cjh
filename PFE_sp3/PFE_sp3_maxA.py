bec9295de286bb0418878eff33f99f7442c31454
cjunho <47020816+cjunho@users.noreply.github.com>
2023-08-13 06:14:18 +0900

    Update PFE_sp3_maxA.py

--- PFE_sp3/PFE_sp3_maxA.py
+++ PFE_sp3/PFE_sp3_maxA.py
@@ -9,9 +9,8 @@ fileE = 'C:/Users/amtjch/Desktop/pfe_sp3/pfe_sp3_short/potflow3dperenergy.txt'
 outputE = open(fileE,'r')
 #  infile.readline() # skip the first line not first line here (yet)
 tijd = []
-# EPot = []
-# EKin = []
 max_data = []
+A_data = []
 
 for line in outputE:
     words = line.split()
@@ -19,11 +18,13 @@ for line in outputE:
     tijd.append(float(words[0]))
     
     max_data.append(float(words[4]))
+    A_data.append(float(words[5]))
 
 
 
 tt10=np.array(tijd)
 max10=np.array(max_data)
+A10=np.array(A_data)
 outputE.close()
 
 
@@ -31,16 +32,12 @@ qq=int(50)
 lqq=int(len(max10)/qq)
 max1=np.zeros((lqq+1,))
 max1[0]=max10[0]
-
+A1[0]=A10[0]
 
 for jj in range(lqq):
     max1[jj+1]=sum(max10[jj*qq:(jj+1)*qq])/qq
+    A1[jj+1]=sum(A10[jj*qq:(jj+1)*qq])/qq
 t1=np.linspace(tt10[0],tt10[-1],lqq+1)
-
-
-
-df = pd.read_csv("C:/Users/amtjch/Desktop/pfe_sp3/pfe_sp3_short/maxA13000.csv")
-maxA = df.to_numpy()[:,1]
 
 
 
@@ -48,7 +45,7 @@ maxA = df.to_numpy()[:,1]
 
 plt.figure(figsize=(10,6))
 
-plt.plot(t1,max1/maxA,label=r'CG2', linewidth='4')
+plt.plot(t1,max1/A1,label=r'CG2', linewidth='4')
 plt.xlabel(' $t (s)$ ',size=16)
 plt.legend(loc='lower right',fontsize="14")
 plt.ylabel( '$max/A$ ',size=16)
