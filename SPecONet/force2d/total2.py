912a6a5d03f7e75f80e9df52f7142c708e3c02ba
Junho Choi <junho_choi@kaist.ac.kr>
2025-12-12 10:02:24 +0900

    change

--- SPecONet/force2d/total2.py
+++ SPecONet/force2d/total2.py
@@ -24,8 +24,8 @@ forcing='sigma5'
 if os.path.isdir(data_path) == False: 
    os.makedirs(data_path)
    try:
-       subprocess.run(f'python ns_solver.py --case train --Nsamples 600 --Ntimes 3 --Equation {Equation}', shell=True)
-       subprocess.run(f'python ns_solver.py --case test --Nsamples 100 --Ntimes 3 --Equation {Equation}', shell=True)
+       subprocess.run(f'python ns_solver.py --case train --Nsamples 600 --Ntimes 10 --Equation {Equation}', shell=True)
+       subprocess.run(f'python ns_solver.py --case test --Nsamples 100 --Ntimes 10 --Equation {Equation}', shell=True)
        print("Script executed successfully.")
        
    except subprocess.CalledProcessError:
@@ -35,23 +35,23 @@ if os.path.isdir(data_path) == False:
     
 try:
     subprocess.run(f'python training2alp.py --blocks 0 --file 600N23 --ks 9 --filters 10'\
-                    f' --epochs 50 --dt 0.01 --forcing {forcing} --ndt 1 --eps 0.1 --kind {kind}', shell=True)
+                    f' --epochs 50000 --dt 0.01 --forcing {forcing} --ndt 1 --eps 0.1 --kind {kind}', shell=True)
     
     df1 = pd.read_csv(filename+f"/call1_alp.csv")
     
     PATH=df1['path'][0]
     subprocess.run(f'python training2alp.py --blocks 0 --file 600N23 --ks 9 --filters 10'\
-                    f' --epochs 20 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --kind {kind} --pretrained true', shell=True)
+                    f' --epochs 20000 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --kind {kind} --pretrained true', shell=True)
     
     df1 = pd.read_csv(filename+f"/call1_alp.csv")
     PATH=df1['path'][0]
     subprocess.run(f'python training2pressure.py --blocks 0 --file 600N23 --kind {kind}'\
-                f' --epochs 10 --ks 9 --filters 10 --dt 0.01 --forcing {forcing} --ndt 1 --eps 0.1 --path {PATH} --order 1', shell=True)
+                f' --epochs 10000 --ks 9 --filters 10 --dt 0.01 --forcing {forcing} --ndt 1 --eps 0.1 --path {PATH} --order 1', shell=True)
     
     df2 = pd.read_csv(filename+f"/call1_pp.csv")    
     PATH2=df2['path'][0]
     subprocess.run(f'python training2pressure2.py --blocks 0 --file 600N23 --kind {kind}'\
-                f' --epochs 10 --ks 9 --filters 10 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --path2 {PATH2} --order 1', shell=True)
+                f' --epochs 10000 --ks 9 --filters 10 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --path2 {PATH2} --order 1', shell=True)
     
     
     print("Script executed successfully.")
@@ -63,7 +63,7 @@ df1 = pd.read_csv(filename+f"/call1_alp.csv")
 PATH_alp=df1['path'][0]
 
 
-for ii in range(2,2+1):
+for ii in range(2,10+1):
     
     df1 = pd.read_csv(filename+f"/call{ii-1}_pp.csv")
     PATH=df1['path'][0]
@@ -75,7 +75,7 @@ for ii in range(2,2+1):
         
         try:
             subprocess.run(f'python training3alp.py --blocks 0 --file 600N23 --ks 9 --filters 10'\
-                            f' --epochs 50 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --order {ORDER} --kind {kind}', shell=True)
+                            f' --epochs 50000 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --order {ORDER} --kind {kind}', shell=True)
             print("Script executed successfully.")
             df1 = pd.read_csv(filename+f"/call{ii}_alp.csv")
             PATH_alp=df1['path'][0]
@@ -84,18 +84,18 @@ for ii in range(2,2+1):
     try:
             
             subprocess.run(f'python training3alp.py --blocks 0 --file 600N23 --ks 9 --filters 10'\
-                            f' --epochs 20 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --path_alp {PATH_alp} --order {ORDER} --kind {kind} --pretrained true', shell=True)
+                            f' --epochs 20000 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --path_alp {PATH_alp} --order {ORDER} --kind {kind} --pretrained true', shell=True)
             
             df1 = pd.read_csv(filename+f"/call{ii}_alp.csv")   
             PATH=df1['path'][0]
             ORDER=df1['order'][0]
             subprocess.run(f'python training2pressure.py --blocks 0 --file 600N23 --kind {kind}'\
-                    f' --epochs 10 --ks 9 --filters 10 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --order {ORDER}', shell=True)
+                    f' --epochs 10000 --ks 9 --filters 10 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --order {ORDER}', shell=True)
             
             df2 = pd.read_csv(filename+f"/call{ii}_pp.csv")    
             PATH2=df2['path'][0]
             subprocess.run(f'python training2pressure2.py --blocks 0 --file 600N23 --kind {kind}'\
-                    f' --epochs 10 --ks 9 --filters 10 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --path2 {PATH2} --order {ORDER}', shell=True)
+                    f' --epochs 10000 --ks 9 --filters 10 --dt 0.01 --forcing {forcing}  --ndt 1 --eps 0.1 --path {PATH} --path2 {PATH2} --order {ORDER}', shell=True)
         
             print("Script executed successfully.")
     except subprocess.CalledProcessError:
